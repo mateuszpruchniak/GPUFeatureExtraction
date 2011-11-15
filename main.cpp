1367878d5c9608d4d892fb8b2196b11c4f913228
Mateusz Pruchniak <mateuszpruchniak@gmail.com>
2011-11-15 14:07:44 +0100

    first

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@
 #include <time.h>
 #include "GPUTransferManager.h"
 #include "GPUImageProcessor.h"
+#include "SIFT.h"
 
+#include "stdafx.h"
 
 
 using namespace std;
@@ -19,37 +21,57 @@ using namespace std;
 
 
 
-int main(int argc, const char** argv)
+// The main function!
+int main()
 {
+	// Create an instance of SIFT
+	SIFT *sift = new SIFT("C:\\car.jpg", 4, 2);
 
+	sift->DoSift();				// Find keypoints
+	sift->ShowAbsSigma();		// Display the sigma table
+	sift->ShowKeypoints();		// Show the keypoints
+	cvWaitKey(0);				// Wait for a keypress
 
-	IplImage* img = cvLoadImage("./img/car.jpg");
-	
-
-	GPUImageProcessor* GPU = new GPUImageProcessor(img->width,img->height,img->nChannels);
-
-
-	
-	GPU->AddProcessing( new Feature("./CL/SIFT.cl",GPU->GPUContext,GPU->Transfer,"ckBuildPyramid") );
-
-
-	GPU->Transfer->CreateBuffers();
-	GPU->Transfer->SendImage(img);
-	GPU->Process();
-	img = GPU->Transfer->ReceiveImage();
-
-	cout << "-------------------------\n\n" << endl;
-
-    cvNamedWindow("sobel", CV_WINDOW_AUTOSIZE); 
-    cvShowImage("sobel", img );
-    cvWaitKey(2);
-
-
-
-	getchar();
-    return 0;
-
+	// Cleanup and exit
+	delete sift;
+	return 0;
 }
 
+
+
+//
+//
+//int main(int argc, const char** argv)
+//{
+//
+//
+//	IplImage* img = cvLoadImage("./img/car.jpg");
+//	
+//
+//	GPUImageProcessor* GPU = new GPUImageProcessor(img->width,img->height,img->nChannels);
+//
+//
+//	
+//	GPU->AddProcessing( new Feature("./CL/SIFT.cl",GPU->GPUContext,GPU->Transfer,"ckBuildPyramid") );
+//
+//
+//	GPU->Transfer->CreateBuffers();
+//	GPU->Transfer->SendImage(img);
+//	GPU->Process();
+//	img = GPU->Transfer->ReceiveImage();
+//
+//	cout << "-------------------------\n\n" << endl;
+//
+//    cvNamedWindow("sobel", CV_WINDOW_AUTOSIZE); 
+//    cvShowImage("sobel", img );
+//    cvWaitKey(2);
+//
+//
+//
+//	getchar();
+//    return 0;
+//
+//}
+//
 
 
